204ee1d49889a44b6a7db8e3c197eb8d04764394
Pratham <20145@iiitu.ac.in>
2022-07-15 02:15:20 +0530

    initial commit

---
+++ android/app/src/main/java/co/appbrewery/quizzler/MainActivity.java
@@ -0,0 +1,15 @@
+package co.appbrewery.quizzler;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+//  }
+//import android.os.Bundle;
+//import io.flutter.app.FlutterActivity;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
